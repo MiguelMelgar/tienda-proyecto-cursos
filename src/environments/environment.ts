1487b43eea7f98d85a3b9edc2dc3014a81766fd8
MIGUEL <miguel.melgar@unas.edu.pe>
2024-10-24 07:40:12 -0500

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    URL_BACKEND: 'http://apis.admincursos-udemy.site/',
+    URL_SERVICIOS: 'http://apis.admincursos-udemy.site/api',
+    URL_FROTEND: 'http://tienda.admincursos-udemy.site',
+};
+
